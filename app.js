fb928a1460e3f50148172a850436847890c8115d
Lei Wang <xwang@sandiego.edu>
2019-06-06 19:57:53 -0700

    implemented drill for caesar cipher

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 
 const app = express();
 
 // This is middleware that requests pass through
 // on their way to the final handler
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 //This is the final request handler
 app.get('/', (req, res) => {
@@ -34,6 +34,27 @@ app.get('/sum', (req, res) => {
     //6. send the response 
     res.send(sum);
 });
+
+app.get('/cipher', (req, res) => {
+    const text = req.query.text;
+    const shift = parseInt(req.query.shift);
+    
+    if (!text) {
+        return res.status(400).send('Please provide some text');
+    }
+    if (!shift || isNaN(shift)) {
+        return res.status(400).send('Please provide a number');
+    }
+
+    let textArray = text.split('');
+    
+    let shiftedArray = textArray.map(letter => letter.charCodeAt(0)     + shift).map(number => String.fromCharCode(number));
+    
+    let finalString = shiftedArray.join('');
+
+    res.send(finalString);
+});
+
 
 app.listen(8000, () => {
     console.log('Express server is listening on port 8000!');
